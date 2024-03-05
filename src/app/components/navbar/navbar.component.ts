5611a72b274234bd97a3a9da20e10a49140c182c
อัษฎาวุธ ไชยรักษ์ <yakukung@MacBookAir.local>
2024-03-05 12:17:02 +0700

    Update all files

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
 // navbar.component.ts
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HttpClient } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-navbar',
+  changeDetection: ChangeDetectionStrategy.Default,
   standalone: true,
   imports: [CommonModule, MatIconModule, MatButtonModule, MatToolbarModule, HttpClientModule],
   templateUrl: './navbar.component.html',
@@ -17,20 +18,21 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class NavbarComponent {
 
-  @Input() usersigndata: any; // Assuming usersigndata is meant to be passed as an input
-
+  @Input() usersigndata: any;
   email: string = '';
   password: string = '';
   first_name: string = '';
   last_name: string = '';
   user_id: string = '';
+  activeButton: string = '';
 
-  constructor(private router: Router, private route: ActivatedRoute, private httpClient: HttpClient) {}
+  currentRoute: string = '';
+
+  constructor(private router: Router, private route: ActivatedRoute, private httpClient: HttpClient, private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
       const user_id = params['user_id'];
-
       if (user_id) {
         this.fetchUserData(user_id);
       }
@@ -47,7 +49,6 @@ export class NavbarComponent {
         this.first_name = response.first_name;
         this.last_name = response.last_name;
         this.user_id = response.user_id;
-
       },
       (error: any) => {
         console.error("Error fetching user data:", error);
@@ -55,14 +56,20 @@ export class NavbarComponent {
     );
   }
 
-
-
+  isButtonActive(route: string): boolean {
+    const isActive = this.router.url.includes(route);
+   
+    return isActive;
+  }
+  
   signup() {
     this.router.navigate(['/']);
   }
+
   profile() {
     this.router.navigate(['/profile'], { queryParams: { user_id: this.user_id } });
   }
+
   ranking() {
     this.router.navigate(['/ranking'], { queryParams: { user_id: this.user_id } });
   }
